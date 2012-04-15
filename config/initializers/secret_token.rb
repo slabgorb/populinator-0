00dc9625456bbf6e83a080ceb71b858dc0d15f71
Keith Avery <kavery@flatworldknowledge.com>
2012-04-14 21:02:31 -0400

    initial rails generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Population::Application.config.secret_token = 'e6f7261be3cff84c08a0c67f45e477acef228f8cb4c2d2ddb56c2f68f85908ac0ccd6ca606e2ffebe2f2e3e00a89443fc85f341b2fa729fff5dc37d5f59b3fb2'
