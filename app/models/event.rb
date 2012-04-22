fce70d26ed131f69f1634eeb2b787bfb42b51b65
Keith Avery <kavery@flatworldknowledge.com>
2012-04-22 16:11:17 -0400

    added seed data to the events and set up proc system

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,6 @@ class Event
   include Mongoid::Document
   field :name, :type => String
   field :description, :type => String
-  field :occurred_on, :type => String
   field :effect, :type => String
   belongs_to :settlement
   belongs_to :being
