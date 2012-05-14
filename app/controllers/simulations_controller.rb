3476a1b62994f808736f991699ec5e1bd211b252
Keith Avery <kavery@flatworldknowledge.com>
2012-05-14 17:02:06 -0400

    commented out some code so the app won't hang

--- app/controllers/simulations_controller.rb
+++ app/controllers/simulations_controller.rb
@@ -18,33 +18,42 @@ class SimulationsController < ApplicationController
 
   
   def run
-    params[:name] ||= 'New Settlement'
-    params[:population] ||= 100
+    params.merge!({ 
+      :name => Settlement.random_name,
+      :population => '100',
+      :years => '100 '           
+    })
     @settlement = Settlement.create(:name => params[:name])
     @settlement.established = 0
-    params[:population].times do
-      @settlement.beings << Person.create
+    params[:population].to_i.times do
+      @settlement.beings << Person.create(:age => 1)
     end
     @settlement.rulers << Ruler.create(:settlement => @settlement)
     @settlement.save
     @output = "Current population: #{@settlement.population}<br/>"
-    100.times do 
-      @year += 1
-      @settlement.beings.each { |b| b.age += 1 }
-      @settlement.beings.each do |person|
-        if rand < 0.10 and person.age > 16 and not person.married?
-          person.marry(person.find_spouse) 
-          @output += "#{person} was married to #{person.spouse}"
-          @output += "#{person} reproduced"
-          # person.reproduce(@settlement.beings.select{|s| s.gender != person.gender }.shuffle.first) if rand < 0.01
-        end
-        if rand < 0.10 then
-          event = Event.disasters.shuffle.first
-          @output += "#{@settlement.name} #{event.description}"
-          event.happened_to(@settlement, rand)
-        end
-        @output += "Current population: #{@settlement.population}<br/>"
+    @year = 1
+    params[:years].to_i.times do
+      @output += "Year: #{@year}"
+      
+      if rand < 0.10 then
+        event = Event.disasters.shuffle.first
+        @output += "#{@settlement.name} #{event.description}"
+        event.happened_to(@settlement, rand)
       end
+    #   @year += 1
+    #   @settlement.beings.each { |b| b.age += 1 }
+    #   @settlement.beings.each do |person|
+    #     if rand < 0.10 and person.age > 16 and not person.married?
+    #       person.marry(person.find_spouse) 
+    #       @output += "#{person} was married to #{person.spouse}"
+    #       @output += "#{person} reproduced"
+    #       # person.reproduce(@settlement.beings.select{|s| s.gender != person.gender }.shuffle.first) if rand < 0.01
+    #     end
+
+    #     @output += "Current population: #{@settlement.population}<br/>"
+    #   end
+      @year += 1
     end
+    render :json => { settlement: @settlement.as_json, history: @output}
   end
 end
