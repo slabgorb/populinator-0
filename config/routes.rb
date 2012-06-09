c743b9606510f9db77a49ca2306ce654f88dde0c
Keith Avery <kavery@flatworldknowledge.com>
2012-06-08 22:02:18 -0400

    routes to support age feature

--- config/routes.rb
+++ config/routes.rb
@@ -16,13 +16,12 @@ Population::Application.routes.draw do
   resources :people, :controller => 'beings',  :_type => 'Person'
   resources :rulers, :controller => 'beings',  :_type => 'Ruler'
   
-  
+  # beings
   get '/beings/graph' => 'beings#graph'
-  
   put '/beings/kill/:id' => 'beings#kill', :as => :kill
   put '/beings/resurrect/:id' => 'beings#resurrect', :as => :resurrect
+  put '/beings/age/:id' => 'beings#age', :as => :age
   put '/beings/reproduce/:parent_a/:parent_b' => 'beings#reproduce', :as => :reproduction
-  
   get '/beings/genotype/:id' => 'beings#genotype'
   
   put '/settlement/seed/:id' => 'settlements#seed', :as => :seed
