766699c23a02578026117f726005339a4181e144
Keith Avery <keith.marshall.avery@gmail.com>
2013-04-27 12:23:12 -0400

    fixed syntax

--- config/routes.rb
+++ config/routes.rb
@@ -15,16 +15,16 @@ Population::Application.routes.draw do
   resources :rulers, :controller => 'beings', _type:'Ruler'
 
   # settlements
-  put '/settlement/seed/:id' => 'settlements#seed', as:seed
+  put '/settlement/seed/:id' => 'settlements#seed', as: :seed
   get '/' => 'settlements#index'
   get '/settlements/random-name' => 'settlements#random_name'
 
   get '/beings/graph' => 'beings#graph'
-  put '/beings/randomize_genetics/:id' => 'beings#randomize_genetics', as:random_genetics
-  put '/beings/kill/:id' => 'beings#kill', as:kill
-  put '/beings/resurrect/:id' => 'beings#resurrect', as:resurrect
-  put '/beings/age/:id' => 'beings#age', as:age
-  put '/beings/reproduce/:parent_a/:parent_b' => 'beings#reproduce', as:reproduction
+  put '/beings/randomize_genetics/:id' => 'beings#randomize_genetics', as: :random_genetics
+  put '/beings/kill/:id' => 'beings#kill', as: :kill
+  put '/beings/resurrect/:id' => 'beings#resurrect', as: :resurrect
+  put '/beings/age/:id' => 'beings#age', as: :age
+  put '/beings/reproduce/:parent_a/:parent_b' => 'beings#reproduce', as: :reproduction
   get '/beings/genotype/:id' => 'beings#genotype'
   get '/beings/family/:id' => 'beings#family'
   get '/beings/description/:id' => 'beings#description'
