c72718faa270bff32ae52bc55ad455572059a9ee
Keith Avery <keith.marshall.avery@gmail.com>
2013-04-27 17:48:03 -0400

    removed comments

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,6 +1,3 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-# {:category => 'life',:name =>'marriage',:description => 'was married', :effect => '{|a,b| a.marry b }'},
-
 FactoryGirl.define do
   factory :event do
     name "nothing"
@@ -8,10 +5,10 @@ FactoryGirl.define do
     description ""
     effect '{|a,b| a,b }'
   end
-  
+
   factory :marriage, :parent => :event do
     name "marriage"
     effect '{|a,b| a.marry b }'
   end
-  
+
 end
