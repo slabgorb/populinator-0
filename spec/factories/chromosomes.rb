b26b7e281f5d3f8e111cba6a16a66edde514a0d9
Keith Avery <keith.marshall.avery@gmail.com>
2013-04-27 17:48:31 -0400

    fixed deprecated factory girl syntax

--- spec/factories/chromosomes.rb
+++ spec/factories/chromosomes.rb
@@ -1,16 +1,13 @@
 
 FactoryGirl.define do
-  factory :chromosome do 
-    after_build do |s|
-      10.times do 
-        s <<  Chromosome.rand_hex 
-      end
-    end
+  factory :chromosome do
+    genes {
+      (Array(lambda{ Chromosome.rand_hex }) * 10).map(&:call)
+    }
   end
-  factory :labrat, :parent => :chromosome do 
-    after_build do |s|
-      s.genes << '01020304A'
-      s.genes << '01CCCCCCC'
-    end
+  factory :labrat, :parent => :chromosome do
+    genes {
+      ['01020304A', '01CCCCCCC']
+    }
   end
 end
