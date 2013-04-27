e754e580b0fbd747843572e6026b5cf26acb9ed8
Keith Avery <keith.marshall.avery@gmail.com>
2013-04-27 18:06:30 -0400

    fixed spec for new factory

--- spec/models/chromosome_spec.rb
+++ spec/models/chromosome_spec.rb
@@ -1,33 +1,33 @@
 require 'spec_helper'
 
 describe Chromosome do
-  before :all do 
-    srand(1) 
+  before :all do
+    srand(1)
     @c0 = FactoryGirl.build(:chromosome)
     @c1 = FactoryGirl.build(:chromosome)
     @c2 = FactoryGirl.build(:chromosome)
   end
-  
-  it "should give the indexed seed" do 
+
+  it "should give the indexed seed" do
     @c0[0].should eq('6AC1F3')
   end
-  
-  
+
+
   context "expression" do
     it "should express the gene" do
       expressions = {
-        hair: { 
+        hair: {
           blond:['01'],
           red:['02'],
           pink:['03'],
           plaid:['FF']
-        } 
+        }
       }
       e = FactoryGirl.build(:labrat)
-      e.express(expressions).should eq({:hair=>{:blond=>2, :red=>1, :pink=>1, :plaid=>0}})
+      e.express(expressions).should eq({:hair=>{:blond=>1, :red=>1, :pink=>1, :plaid=>0}})
     end
   end
-  
+
   context "mutation" do
     before :all do
       @m = FactoryGirl.build(:chromosome)
@@ -39,33 +39,33 @@ describe Chromosome do
       srand(1)
       Chromosome.rand_hex.should eq('6AC1F3')
     end
-    
+
     it "should have a different genome" do
       @old.should_not eq(@m.to_s)
     end
-    
+
     it "should generate a value of the genome" do
-      @m.value.should be(78313645)
+      @m.value.should be(89011217)
     end
-    
+
   end
-  
+
   context "reproduction" do
     before :all do
       srand(1)
       @c3 = @c1.reproduce_with @c2
     end
-    
+
     it 'should have a genome pattern based on the parents' do
       # NOTE: this works because of the call to srand
       @c3[0].should eq(@c2[0])
     end
-    
-    it 'should be a chromosome' do 
+
+    it 'should be a chromosome' do
       @c3.should be_a(Chromosome)
     end
-    
-    it 'should be the same genome length as the parent' do     
+
+    it 'should be the same genome length as the parent' do
       @c3.length.should eq(@c1.length)
     end
   end
