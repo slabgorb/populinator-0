c02637960414411dac5db2ee14f0275856c8c847
Keith Avery <keith.marshall.avery@gmail.com>
2013-04-27 12:17:11 -0400

    language list view

--- app/models/language.rb
+++ app/models/language.rb
@@ -2,6 +2,7 @@ class Language
   include Mongoid::Document
   field :name, type: String
   field :description, type: String
-  field :glossary, type: Array
+  field :glossary, type: Hash
   has_many :histograms
+  has_many :corpora
 end
