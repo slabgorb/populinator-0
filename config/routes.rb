0a5461d708b333efe529b1c113ba00597553f556
Keith Avery <keith.marshall.avery@gmail.com>
2013-04-27 12:13:50 -0400

    constrained histogram and updated hash notation

--- config/routes.rb
+++ config/routes.rb
@@ -7,28 +7,28 @@ Population::Application.routes.draw do
   resources :events
   resources :languages do
     resources :corpora
-    resources :histograms
+    resources :histograms, only: [:show]
   end
 
   # settlements
-  put '/settlement/seed/:id' => 'settlements#seed', :as => :seed
-  resources :rulers, :controller => 'beings',  :_type => 'Ruler'
+  put '/settlement/seed/:id' => 'settlements#seed', as:seed
+  resources :rulers, :controller => 'beings', _type:'Ruler'
   get '/' => 'settlements#index'
   get '/settlements/random-name' => 'settlements#random_name'
   resources :settlements
 
   get '/beings/graph' => 'beings#graph'
-  put '/beings/randomize_genetics/:id' => 'beings#randomize_genetics', :as => :random_genetics
-  put '/beings/kill/:id' => 'beings#kill', :as => :kill
-  put '/beings/resurrect/:id' => 'beings#resurrect', :as => :resurrect
-  put '/beings/age/:id' => 'beings#age', :as => :age
-  put '/beings/reproduce/:parent_a/:parent_b' => 'beings#reproduce', :as => :reproduction
+  put '/beings/randomize_genetics/:id' => 'beings#randomize_genetics', as:random_genetics
+  put '/beings/kill/:id' => 'beings#kill', as:kill
+  put '/beings/resurrect/:id' => 'beings#resurrect', as:resurrect
+  put '/beings/age/:id' => 'beings#age', as:age
+  put '/beings/reproduce/:parent_a/:parent_b' => 'beings#reproduce', as:reproduction
   get '/beings/genotype/:id' => 'beings#genotype'
   get '/beings/family/:id' => 'beings#family'
   get '/beings/description/:id' => 'beings#description'
   get '/beings/history/:id' => 'beings#history'
   get '/people/random-name' => 'people#random_name'
-  resources :people, :controller => 'beings',  :_type => 'Person'
+  resources :people, :controller => 'beings',  _type:'Person'
   # beings
   resources :beings do
     resources :things
