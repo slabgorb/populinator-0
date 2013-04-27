f53fbc4140f12c0659ed16bd97eb8c82d85846a2
Keith Avery <keith.marshall.avery@gmail.com>
2013-04-27 18:07:14 -0400

    new factory girl syntax

--- spec/factories/settlements.rb
+++ spec/factories/settlements.rb
@@ -1,14 +1,13 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :settlement do |s|
-    s.name Settlement.random_name
-    s.after_build do |t|
-      100.times { t.residents << FactoryGirl.create(:person, 
-                                                 surname:%w|Green Red Blue|.shuffle.first, 
-                                                 given_name:%w|Pat Bobby Shawn|.shuffle.first, 
-                                                 gender:['male','female'].shuffle.first) }
-      t.residents << FactoryGirl.create(:ruler)
-    end
+  factory :settlement do
+    name Settlement.random_name
+    residents {
+      Array(1..100).map { FactoryGirl.create(:person,
+                                     surname:%w|Green Red Blue|.shuffle.first,
+                                     given_name:%w|Pat Bobby Shawn|.shuffle.first,
+                                     gender:['male','female'].shuffle.first) } << FactoryGirl.create(:ruler)
+    }
   end
 end
