4c99fe51a8d8a1a6c1bbc1847df0e50d25aec890
Keith Avery <keith.marshall.avery@gmail.com>
2013-04-28 09:51:45 -0400

    added language variable for use in view

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,6 +2,8 @@ class IndexController < ApplicationController
   def homepage
     @settlements = Settlement.all
     @settlement = Settlement.new
+    @languages = Language.all
+    @language = Language.new
   end
 
 end
