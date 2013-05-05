3b0b2632cc6e1b4ef8c7b63fa59c30363e2da4ac
Keith Avery <keith.marshall.avery@gmail.com>
2013-05-05 15:08:11 -0400

    making the forms modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,7 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
+//= require twitter/bootstrap
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
@@ -11,4 +12,3 @@
 //= require_tree .
 //= require best_in_place
 //= require best_in_place.purr
-//= require twitter/bootstrap
