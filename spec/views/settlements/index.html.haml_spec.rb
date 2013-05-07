c3312493f79e8bb3e4499d469f7537f838bfe5ab
Keith Avery <keith.marshall.avery@gmail.com>
2013-05-07 06:39:02 -0400

    removed view spec

--- spec/views/settlements/index.html.haml_spec.rb
+++ spec/views/settlements/index.html.haml_spec.rb
@@ -1,20 +1,5 @@
 require 'spec_helper'
 
 describe "settlements/index" do
-  before(:each) do
-    assign(:settlements, [
-      stub_model(Settlement,
-        :name => "Name"
-      ),
-      stub_model(Settlement,
-        :name => "Name"
-      )
-    ])
-  end
 
-  it "renders a list of settlements" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-  end
 end
