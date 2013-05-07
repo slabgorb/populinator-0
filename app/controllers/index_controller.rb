cba69d0a256e2d294bcea7e7fb666be9f453907d
Keith Avery <keith.marshall.avery@gmail.com>
2013-05-06 20:31:53 -0400

    icon list api

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -7,4 +7,8 @@ class IndexController < ApplicationController
     @corpus = Corpus.new
   end
 
+  def icon_list
+    render json: Dir.glob(File.join(Rails.root, 'app', 'assets', 'images', 'heraldry') + '/*.png').map{|m| File.basename(m).gsub(/\.png/,'')}
+  end
+
 end
