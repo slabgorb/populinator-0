cf30d2f1b0943c78e346fa76acf39369f33190b8
Keith Avery <keith.marshall.avery@gmail.com>
2013-05-11 22:13:08 -0400

    another slight reskin

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -1,2 +1,8 @@
 module IndexHelper
+  def flat_button(img, link, color='#FF0000', label='')
+    %Q|
+       <div class='icon' rel='#{link}' style='background-color:#{color};'>
+          <a  href='#{link}'>#{label}</a>
+          <a  href='#{link}'><img src='/assets/icons/#{img}.png'/></a></div>|.html_safe
+  end
 end
