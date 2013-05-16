ebec573fd4f299e24be0113eb9ae498840924c38
Keith Avery <keith.marshall.avery@gmail.com>
2013-05-16 18:00:42 -0400

    fixed histogram data structure

--- app/models/corpus.rb
+++ app/models/corpus.rb
@@ -15,17 +15,22 @@ class Corpus
   # TODO: make this work even if pointed to a large file
   #
   def compile_histogram
-    text = Net::HTTP.get(URI('http://' + url.gsub(/http:\/\//,''))).gsub(/[!@#\$%^&*\(\)-_=+{}\[\]|\\?\/.,0-9]/,' ').downcase
-    histo = Hash.new {|h,k| h[k] = []}
+    histo = Hash.new
     key = [@@start_token] * lookback
-    text.split(//).each do |char|
+    get_text.split(//).each do |char|
       key.push(char).shift
-      histo[key] ||= Hash.new(0)
-      histo[key][char] += 1
+      histo[key.clone] ||= { }
+      histo[key.clone][char] ||= 0
+      histo[key.clone][char] += 1
     end
+    p histo
     update_attribute(:histogram, histo.to_json)
   end
 
+
+  def get_text
+    Net::HTTP.get(URI('http://' + url.gsub(/http:\/\//,''))).gsub(/[\b\s!@#\$%^&*\(\)-_=+{}\[\]|\\?\/.,0-9]/,' ').downcase
+  end
 
   def data
     JSON.parse(histogram)
