992a66e95dc32c5781892b09f90f82a6c53cc449
Keith Avery <keith.marshall.avery@gmail.com>
2013-05-16 21:04:34 -0400

    histogram better, data being memoized

--- app/models/corpus.rb
+++ app/models/corpus.rb
@@ -1,4 +1,4 @@
-class Corpus
+class Corpus < Hash
   include Mongoid::Document
   field :name, type: String
   field :url, type: String
@@ -7,41 +7,42 @@ class Corpus
   field :lookback, type: Integer
   belongs_to :language
 
-  @@start_token = :start
-  @@end_token = :end
+  @@start_token = '^'
+  @@end_token = '$'
 
   ##
-  # Reads the file from the web and converts it into an array
-  # TODO: make this work even if pointed to a large file
+  # Compiles the letter probability data structure.
   #
   def compile_histogram
     histo = Hash.new
     key = [@@start_token] * lookback
-    get_text.split(//).each do |char|
-      key.push(char).shift
-      histo[key.clone] ||= { }
-      histo[key.clone][char] ||= 0
+    get_text.each do |char|
+      ((histo[key.clone] ||= { })[char] ||= 0)
       histo[key.clone][char] += 1
+      key.push(char).shift
     end
-    p histo
     update_attribute(:histogram, histo.to_json)
   end
 
-
+  ##
+  # Reads the file from the web and converts it into an array
+  # @see Corpus#compile_histogram
+  # TODO: make this work even if pointed to a large file
+  #
   def get_text
-    Net::HTTP.get(URI('http://' + url.gsub(/http:\/\//,''))).gsub(/[\b\s!@#\$%^&*\(\)-_=+{}\[\]|\\?\/.,0-9]/,' ').downcase
+    Net::HTTP.get(URI('http://' + url.gsub(/http:\/\//,''))).gsub(/[ !@#\$%^&*\(\)-_=+{}\[\]|\\?\/.,0-9]/,' ').downcase.gsub(/\s/, @@end_token).split(//)
   end
 
-  def data
-    JSON.parse(histogram)
+  ##
+  # Deserialize the json-format histogram
+  #
+  def data(force = false)
+    @data = nil if force
+    @data ||= JSON.parse(histogram)
   end
 
-  def +(histo)
-    h = data
-    histo.each_pair do |k,v|
-      h[k.split(//)] += v
-    end
-    h
+  def +(corpus)
+    data.deep_merge(corpus.data)
   end
 
 end
