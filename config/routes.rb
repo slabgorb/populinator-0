765b8fbb1f2e3b1c9acd5b6665815ab87f4d5fb9
Keith Avery <keith.marshall.avery@gmail.com>
2013-05-18 13:27:08 -0400

    new routes to allow for ruler resource pathing

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Population::Application.routes.draw do
   resources :languages
   resources :settlements
   resources :corpora
+  resources :rulers, path: 'beings'
 
   # settlements
   get '/settlement/random' => 'settlements#create', random: true, as: :random_settlement
@@ -19,15 +20,16 @@ Population::Application.routes.draw do
 
 
   get '/beings/graph' => 'beings#graph'
-  put '/beings/randomize_genetics/:id' => 'beings#randomize_genetics', as: :random_genetics
   put '/beings/kill/:id' => 'beings#kill', as: :kill
   put '/beings/resurrect/:id' => 'beings#resurrect', as: :resurrect
   put '/beings/age/:id' => 'beings#age', as: :age
   match '/beings/reproduce/:parent_a/:parent_b' => 'beings#reproduce', as: :reproduction
-  get '/beings/genotype/:id' => 'beings#genotype'
+  get '/beings/:id/genotype/' => 'beings#genotype'
+  match '/beings/:id/genotype/random' => 'beings#randomize_genetics', as: :random_genotype
   get '/beings/family/:id' => 'beings#family'
   get '/beings/description/:id' => 'beings#description'
   get '/beings/history/:id' => 'beings#history'
+  get '/beings/name/random' => 'people#random_name'
   get '/people/random-name' => 'people#random_name'
   resources :people, :controller => 'beings',  _type:'Person'
   # beings
