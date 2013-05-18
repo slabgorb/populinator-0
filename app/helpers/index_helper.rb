00bb67ff31af92754ee6fb45b420a17dbde93b93
Keith Avery <keith.marshall.avery@gmail.com>
2013-05-18 11:03:28 -0400

    changes to flat button to make the look a little clearer

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -4,7 +4,7 @@ module IndexHelper
     label = options[:label] || ''
     size = options[:size] || 60
     %Q|
-        <div class='flat-button tip' rel='.tooltip' href='#{link}' title='#{label}' style='background-color:#{background_color};'>
+        <div class='flat-button tip' rel='.tooltip' href='#{link}' title='#{label}' style='width:#{size}px; height:#{size}px; background-color:#{background_color};'>
           <a  href='#{link}'><img src='/assets/heraldry/#{size}/#{img}.png'/></a>
         </div>
       |.html_safe
